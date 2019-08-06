979cb3daee8f1a1471827a7046b69a600657590b
Yichen <ycsi@umich.edu>
2019-08-06 13:49:06 -0400

    Change to CMake

---
+++ CMakeFiles/bluebear.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/net/wonderland/home/ycsi/tool/bluebear/src/Error.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/Error.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/bcf_ordered_reader.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/bcf_ordered_reader.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/bcf_ordered_writer.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/bcf_ordered_writer.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/bluebear.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/bluebear.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_ibs0_baseline.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_baseline.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_ibs0_flanking.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_flanking.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_ibs0_pairwise.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_pairwise.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_ibs0_unconditional.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_unconditional.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_ibs0_view.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_view.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/cmd_vcf_raresharing.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/cmd_vcf_raresharing.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/commands.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/commands.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/compact_matrix.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/compact_matrix.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/filter.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/filter.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/genome_interval.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/genome_interval.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/genotype_concordance.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/genotype_concordance.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/hts_utils.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/hts_utils.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/ibs0_phase.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/ibs0_phase.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/interval.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/interval.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/interval_tree.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/interval_tree.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/params.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/params.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/pbwt_prefix.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/pbwt_prefix.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/pbwt_suffix.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/pbwt_suffix.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/reference_sequence.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/reference_sequence.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/tsv_reader.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/tsv_reader.cpp.o"
+  "/net/wonderland/home/ycsi/tool/bluebear/src/utils.cpp" "/net/wonderland/home/ycsi/tool/bluebear/CMakeFiles/bluebear.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../htslib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/bluebear.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bluebear.dir/src/Error.cpp.o"
+  "CMakeFiles/bluebear.dir/src/bcf_ordered_reader.cpp.o"
+  "CMakeFiles/bluebear.dir/src/bcf_ordered_writer.cpp.o"
+  "CMakeFiles/bluebear.dir/src/bluebear.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_baseline.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_flanking.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_pairwise.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_unconditional.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_ibs0_view.cpp.o"
+  "CMakeFiles/bluebear.dir/src/cmd_vcf_raresharing.cpp.o"
+  "CMakeFiles/bluebear.dir/src/commands.cpp.o"
+  "CMakeFiles/bluebear.dir/src/compact_matrix.cpp.o"
+  "CMakeFiles/bluebear.dir/src/filter.cpp.o"
+  "CMakeFiles/bluebear.dir/src/genome_interval.cpp.o"
+  "CMakeFiles/bluebear.dir/src/genotype_concordance.cpp.o"
+  "CMakeFiles/bluebear.dir/src/hts_utils.cpp.o"
+  "CMakeFiles/bluebear.dir/src/ibs0_phase.cpp.o"
+  "CMakeFiles/bluebear.dir/src/interval.cpp.o"
+  "CMakeFiles/bluebear.dir/src/interval_tree.cpp.o"
+  "CMakeFiles/bluebear.dir/src/params.cpp.o"
+  "CMakeFiles/bluebear.dir/src/pbwt_prefix.cpp.o"
+  "CMakeFiles/bluebear.dir/src/pbwt_suffix.cpp.o"
+  "CMakeFiles/bluebear.dir/src/reference_sequence.cpp.o"
+  "CMakeFiles/bluebear.dir/src/tsv_reader.cpp.o"
+  "CMakeFiles/bluebear.dir/src/utils.cpp.o"
+  "bin/bluebear.pdb"
+  "bin/bluebear"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bluebear.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
